5a2609a428b17c311fa00b759dd95a3bfa3cb0f6
shramp <seamusmurchadh@gmail.com>
2019-03-08 10:14:48 -0500

    resolved issue, updated css/js cdn tags

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-    
+
+    // Hover -------------------------------------------------------------------
+    $("#mountains").hover(
+        function() {
+            $(this).text("| I come completely out of the mountains |");
+        },
+        function() {
+            $(this).text("| Ég kem alveg af fjöllum |");
+    });
+
+    $("#resumeLink").hover(
+        function() {
+            $(this).text(" My Story");
+        },
+        function() {
+            $(this).text(" Sagan Mín");
+        }
+    );
+
+    $("#gitLink").hover(
+        function() {
+            $(this).text(" Bootcamp Projects");
+        },
+        function() {
+            $(this).text(" Æfingabúðir Verkefni");
+        }
+    );
+
 });
